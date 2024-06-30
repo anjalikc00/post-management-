6d4688aadb04e15f0d018e699e37f7b03c09880a
AnjaliKhatrichhetri <chhetrianjali31@gmail.com>
2024-06-30 19:56:16 +0545

    models added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose=require('mongoose')
 mongoose.connect("mongodb://127.0.0.1:27017/user-roles-perm")
 const express=require("express")
 const app=express();
+app.use(express.json())
 app.use(express.static('public'))
 const port=process.env.SERVER_PORT || 8000;
 app.listen( port,()=>{
